c60a162aace1ed57fda12d731245b9dd9bd38171
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-11-12 09:13:25 -0300

    feat: add quicksort::three_way_bentley_mcilroy
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     algorithms::sorting::quicksort::lomuto_clrs::example();
     algorithms::sorting::quicksort::classic_hoare::example();
     algorithms::sorting::quicksort::three_way::example();
+    algorithms::sorting::quicksort::three_way_bentley_mcilroy::example();
 }
--- algorithms/src/sorting/quicksort/mod.rs
+++ algorithms/src/sorting/quicksort/mod.rs
@@ -1,3 +1,4 @@
 pub mod classic_hoare;
 pub mod lomuto_clrs;
 pub mod three_way;
+pub mod three_way_bentley_mcilroy;
