59865fb2ffeacf43bf6508aaee1ff4b100cde00c
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2024-11-23 13:03:44 -0300

    feat: leetcode problems (238,1207,2390)
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

---
+++ problems/src/leetcode/_0002390_removing_stars_from_a_string.rs
@@ -0,0 +1,81 @@
+// 0002390. Removing Stars From a String
+// https://leetcode.com/problems/removing-stars-from-a-string/description/
+// You are given a string s, which contains stars *.
+//
+// In one operation, you can:
+//
+// - Choose a star in s.
+// - Remove the closest non-star character to its left, as well as remove the star itself.
+//
+// Return the string after all stars have been removed.
+//
+// Note:
+//
+//     The input will be generated such that the operation is always possible.
+//     It can be shown that the resulting string will always be unique.
+//
+// Example 1:
+//
+// Input: s = "leet**cod*e"
+// Output: "lecoe"
+// Explanation: Performing the removals from left to right:
+// - The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+// - The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+// - The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+// There are no more stars, so we return "lecoe".
+//
+// Example 2:
+//
+// Input: s = "erase*****"
+// Output: ""
+// Explanation: The entire string is removed, so we return an empty string.
+//
+// Constraints:
+//
+//     1 <= s.length <= 105
+//     s consists of lowercase English letters and stars *.
+//     The operation above can be performed on s.
+//
+// ---
+pub fn remove_stars(s: String) -> String {
+    let mut stack: String = String::with_capacity(s.len());
+    for c in s.chars() {
+        if c == '*' {
+            stack.pop();
+        } else {
+            stack.push(c);
+        }
+    }
+    stack
+}
+// ---
+
+pub fn testcase() {
+    let s = String::from("leet**cod*e");
+    let result = remove_stars(s);
+    eprintln!(
+        "leetcode/0002390_removing_stars_from_a_string: {:?}",
+        result
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let s = String::from("leet**cod*e");
+        let result = remove_stars(s);
+        let expected = String::from("lecoe");
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_2() {
+        let s = String::from("erase*****");
+        let result = remove_stars(s);
+        let expected = String::from("");
+        assert_eq!(result, expected);
+    }
+}
--- problems/src/main.rs
+++ problems/src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
     problems::codewars::_5667e8f4e3f572a8f2000039_mumbling::testcase();
     problems::leetcode::_0000151_reverse_words_in_a_string::testcase();
+    problems::leetcode::_0000238_product_of_array_except_self::testcase();
     problems::leetcode::_0000345_reverse_vowels_of_a_string::testcase();
     problems::leetcode::_0000605_can_place_flowers::testcase();
     problems::leetcode::_0000724_find_pivot_index::testcase();
+    problems::leetcode::_0001207_unique_number_of_occurrences::testcase();
     problems::leetcode::_0001431_kids_with_the_greatest_number_of_candies::testcase();
     problems::leetcode::_0001768_merge_strings_alternately::testcase();
     problems::leetcode::_0002215_find_the_difference_of_two_arrays::testcase();
+    problems::leetcode::_0002390_removing_stars_from_a_string::testcase();
 }
