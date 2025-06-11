60520bafa3f41d2936d8028ca4914988f5f6468a
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2025-06-11 14:53:26 -0300

    chore: Improve leetcode 35
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- problems/src/leetcode/_0000035_search_insert_position.rs
+++ problems/src/leetcode/_0000035_search_insert_position.rs
@@ -31,33 +31,6 @@
 pub fn search_insert_1(nums: Vec<i32>, target: i32) -> i32 {
     let l = nums.len();
     let mut lo = 0;
-    let mut hi = l - 1;
-
-    while lo <= hi {
-        let mid = (lo + hi) / 2;
-
-        if nums[mid] == target {
-            return mid as i32;
-        }
-
-        if nums[mid] < target {
-            lo = mid + 1
-        }
-
-        if nums[mid] > target && mid == 0 {
-            break;
-        }
-
-        if nums[mid] > target {
-            hi = mid - 1
-        }
-    }
-    lo as i32
-}
-
-pub fn search_insert_2(nums: Vec<i32>, target: i32) -> i32 {
-    let l = nums.len();
-    let mut lo = 0;
     let mut mid = l / 2;
 
     while mid >= 1 {
@@ -74,7 +47,7 @@ pub fn search_insert_2(nums: Vec<i32>, target: i32) -> i32 {
     }
 }
 
-pub fn search_insert_3(nums: Vec<i32>, target: i32) -> i32 {
+pub fn search_insert_2(nums: Vec<i32>, target: i32) -> i32 {
     let mut lo = 0;
     let mut hi = nums.len();
 
@@ -90,11 +63,11 @@ pub fn search_insert_3(nums: Vec<i32>, target: i32) -> i32 {
     lo as i32
 }
 
-pub fn search_insert_4(nums: Vec<i32>, target: i32) -> i32 {
+pub fn search_insert_3(nums: Vec<i32>, target: i32) -> i32 {
     nums.binary_search(&target).unwrap_or_else(|x| x) as i32
 }
 
-pub fn search_insert_5(nums: Vec<i32>, target: i32) -> i32 {
+pub fn search_insert_4(nums: Vec<i32>, target: i32) -> i32 {
     nums.partition_point(|&num| num < target) as i32
 }
 // ---
@@ -114,10 +87,6 @@ pub fn testcase() {
 
     let nums = vec![-1, 0, 1, 2, 3, 4, 5, 6, 8, 9, 10];
     let res = search_insert_4(nums, 7);
-    eprintln!("{} {:?}", module_path!(), res);
-
-    let nums = vec![-1, 0, 1, 2, 3, 4, 5, 6, 8, 9, 10];
-    let res = search_insert_5(nums, 7);
     eprintln!("{} {:?}", module_path!(), res);
 }
 
@@ -328,59 +297,6 @@ mod tests_4 {
     fn test_6() {
         let nums = vec![1, 3];
         let res = search_insert_4(nums, 4);
-        let expected = 2;
-        assert_eq!(res, expected);
-    }
-}
-
-#[cfg(test)]
-mod tests_5 {
-    use super::*;
-
-    #[test]
-    fn test_1() {
-        let nums = vec![1, 3, 5, 6];
-        let res = search_insert_5(nums, 5);
-        let expected = 2;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_2() {
-        let nums = vec![1, 3, 5, 6];
-        let res = search_insert_5(nums, 2);
-        let expected = 1;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_3() {
-        let nums = vec![1, 3, 5, 6];
-        let res = search_insert_5(nums, 7);
-        let expected = 4;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_4() {
-        let nums = vec![-1, 0, 1, 2, 3, 4, 5, 6, 8, 9, 10];
-        let res = search_insert_5(nums, 7);
-        let expected = 8;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_5() {
-        let nums = vec![1, 3, 5, 6];
-        let res = search_insert_5(nums, 0);
-        let expected = 0;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn test_6() {
-        let nums = vec![1, 3];
-        let res = search_insert_5(nums, 4);
         let expected = 2;
         assert_eq!(res, expected);
     }
