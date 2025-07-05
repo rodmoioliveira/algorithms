d0873e2b6336147e018c4b581e356718851e7b65
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2025-07-05 19:49:08 -0300

    chore: Add new approach to leetcode 1657
    Signed-off-by: Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>

--- problems/src/leetcode/_0001657_determine_if_two_strings_are_close.rs
+++ problems/src/leetcode/_0001657_determine_if_two_strings_are_close.rs
@@ -39,18 +39,18 @@
 //     1 <= word1.length, word2.length <= 105
 //     word1 and word2 contain only lowercase English letters.
 // ---
-pub fn close_strings(word1: String, word2: String) -> bool {
+pub fn close_strings_hashmap(word1: String, word2: String) -> bool {
     use std::collections::{HashMap, HashSet};
 
     let mut w1: HashMap<char, usize> = HashMap::new();
     let mut w2: HashMap<char, usize> = HashMap::new();
 
-    for l in word1.chars() {
-        w1.entry(l).and_modify(|v| *v += 1).or_insert(1);
+    for c in word1.chars() {
+        w1.entry(c).and_modify(|v| *v += 1).or_insert(1);
     }
 
-    for l in word2.chars() {
-        w2.entry(l).and_modify(|v| *v += 1).or_insert(1);
+    for c in word2.chars() {
+        w2.entry(c).and_modify(|v| *v += 1).or_insert(1);
     }
 
     let w1_keys: HashSet<&char> = HashSet::from_iter(w1.keys());
@@ -63,10 +63,37 @@ pub fn close_strings(word1: String, word2: String) -> bool {
 
     w1_keys == w2_keys && w1_values == w2_values
 }
+
+pub fn close_strings_bitwise(word1: String, word2: String) -> bool {
+    let mut bw1: u32 = 0;
+    let mut bw2: u32 = 0;
+
+    let mut fw1: [usize; 26] = [0; 26];
+    let mut fw2: [usize; 26] = [0; 26];
+
+    let a = b'a';
+
+    for c in word1.bytes() {
+        let index = c - a;
+        fw1[index as usize] += 1;
+        bw1 |= 1 << index;
+    }
+
+    for c in word2.bytes() {
+        let index = c - a;
+        fw2[index as usize] += 1;
+        bw2 |= 1 << index;
+    }
+
+    fw1.sort();
+    fw2.sort();
+
+    bw1 == bw2 && fw1 == fw2
+}
 // ---
 
 pub fn testcase() {
-    let res = close_strings(
+    let res = close_strings_bitwise(
         "aaabbbbccddeeeeefffff".to_owned(),
         "aaaaabbcccdddeeeeffff".to_owned(),
     );
@@ -74,33 +101,69 @@ pub fn testcase() {
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_hasmap {
     use super::*;
 
     #[test]
     fn test_1() {
-        let res = close_strings("abc".to_owned(), "bca".to_owned());
+        let res = close_strings_hashmap("abc".to_owned(), "bca".to_owned());
         let expected = true;
         assert_eq!(res, expected);
     }
 
     #[test]
     fn test_2() {
-        let res = close_strings("a".to_owned(), "aa".to_owned());
+        let res = close_strings_hashmap("a".to_owned(), "aa".to_owned());
         let expected = false;
         assert_eq!(res, expected);
     }
 
     #[test]
     fn test_3() {
-        let res = close_strings("cabbba".to_owned(), "abbccc".to_owned());
+        let res = close_strings_hashmap("cabbba".to_owned(), "abbccc".to_owned());
         let expected = true;
         assert_eq!(res, expected);
     }
 
     #[test]
     fn test_4() {
-        let res = close_strings(
+        let res = close_strings_hashmap(
+            "aaabbbbccddeeeeefffff".to_owned(),
+            "aaaaabbcccdddeeeeffff".to_owned(),
+        );
+        let expected = false;
+        assert_eq!(res, expected);
+    }
+}
+
+#[cfg(test)]
+mod tests_bitwise {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let res = close_strings_bitwise("abc".to_owned(), "bca".to_owned());
+        let expected = true;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_2() {
+        let res = close_strings_bitwise("a".to_owned(), "aa".to_owned());
+        let expected = false;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_3() {
+        let res = close_strings_bitwise("cabbba".to_owned(), "abbccc".to_owned());
+        let expected = true;
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn test_4() {
+        let res = close_strings_bitwise(
             "aaabbbbccddeeeeefffff".to_owned(),
             "aaaaabbcccdddeeeeffff".to_owned(),
         );
